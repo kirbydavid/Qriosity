8b75ca91ce647a439d7b8e07e6ebae5ea5100a37
Jan Manuel Murillo <149052738+7ckngmad0@users.noreply.github.com>
2024-08-24 01:44:46 +0800

    Add files via upload

--- LandingPage/landing.js
+++ LandingPage/landing.js
@@ -1,3 +1,42 @@
+         // SignIn&SignUpBtn
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+         const popupContainer = document.getElementById('popupContainer');
+
+        signUpButton.addEventListener('click', () => {
+        popupContainer.classList.add("right-panel-active");
+         });
+
+        signInButton.addEventListener('click', () => {
+        popupContainer.classList.remove("right-panel-active");
+         });
+
+         document.getElementById('signIn').addEventListener('click', function() {
+            changeCloseButtonColor('dark');
+         });
+         
+         document.getElementById('signUp').addEventListener('click', function() {
+            changeCloseButtonColor('light');
+         });
+         
+         function changeCloseButtonColor(theme) {
+            const closeBtn = document.getElementById('closeBtn');
+            if (theme === 'dark') {
+                closeBtn.style.color = '#ffffff'; // White for dark background
+            } else {
+                closeBtn.style.color = '#000000'; // Black for light background
+            }
+         }
+         
+         document.getElementById("Generate").addEventListener("click", function() {
+            window.location.href = "../Products/Product.html"; // Replace with the actual path
+          });
+          
+        
+         // Initial color setup
+         changeCloseButtonColor('dark'); // or 'light'
+         
+
         // Trigger pop-up when "Log in" is clicked
         document.getElementById('Login').addEventListener('click', function(event) {
             event.preventDefault();
